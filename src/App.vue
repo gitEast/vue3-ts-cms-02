86bd8c92e051ff233a4d1c380227ac75083cfb61
East <linxiayoudongfeng@gmail.com>
2021-11-24 19:45:52 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,6 @@ import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-
-
-
-
-
-
-
   name: 'App',
   components: {
     HelloWorld
