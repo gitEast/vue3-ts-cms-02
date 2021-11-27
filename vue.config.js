b7e637cafe9e686f17b0f5d53120127253dd0267
East <linxiayoudongfeng@gmail.com>
2021-11-27 17:56:52 +0800

    fix(未使用的东西注释掉): 未使用的东西注释掉

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 /*
  * @Author: East
  * @Date: 2021-11-25 10:23:26
- * @LastEditTime: 2021-11-26 13:36:41
+ * @LastEditTime: 2021-11-27 17:55:51
  * @LastEditors: Please set LastEditors
  * @Description: 修改 Vue CLI 配置
  * @FilePath: \vue3-ts-cms-02\vue.config.js
  */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+// const Components = require('unplugin-vue-components/webpack')
+// const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
   outputDir: './build', // 打包的输出文件夹
